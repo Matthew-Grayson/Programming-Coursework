5f2b228ffc21b19d18768d633dd34a0e55bea183
Matthew Grayson <matthew.grayson@associates.cisa.dhs.gov>
2025-09-21 22:05:39 -0500

    Add HelloWorld to COS-1110 coursework directory.

---
+++ COS-1110/HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+    A simple Java program that prints "Hello, World" to the terminal.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Prints "Hello, World" in the terminal.
+        System.out.println("Hello, World");
+    }
+
+}
