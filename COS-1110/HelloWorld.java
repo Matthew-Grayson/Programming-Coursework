171baf746a3d29125cf47b4c0509b3a13add5e72
Matthew Grayson <matthew.grayson@associates.cisa.dhs.gov>
2025-09-21 22:36:08 -0500

    Add variable assignment/use, comments, for loop, and delay to HelloWorld.

--- COS-1110/HelloWorld.java
+++ COS-1110/HelloWorld.java
@@ -4,7 +4,27 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
+        // Declare a byte variable with an integer < 128
+        byte smallNumber = 5;
 
+        // Countdown from the byte variable to 1 with a 1 second delay between each number
+        for (int i = 0; i < smallNumber; i++) {
+            System.out.print(smallNumber - i);
+            // Try-catch block to handle  InterruptedException
+            try {
+                // Delay for 1 second and display a dot for each third of a second
+                System.out.print(".");
+                Thread.sleep(333);
+                System.out.print(".");
+                Thread.sleep(333);
+                System.out.print(".");
+                Thread.sleep(334);
+                System.out.println("");
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         // Prints "Hello, World" in the terminal.
         System.out.println("Hello, World");
     }
