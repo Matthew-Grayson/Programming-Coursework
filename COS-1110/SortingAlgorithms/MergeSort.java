3bbab44be3f5e09f80a78ccd2bf9f64ea11d042d
Matthew Grayson <matthew.grayson@associates.cisa.dhs.gov>
2025-09-23 19:31:35 -0500

    Add MergeSort class.

---
+++ COS-1110/SortingAlgorithms/MergeSort.java
@@ -0,0 +1,81 @@
+package SortingAlgorithms;
+
+public class MergeSort {
+    void sort(int arr[], int left, int right)
+    {
+        if (left < right)
+        {
+            int mid = (left+right)/2;
+            sort(arr, left, mid);
+            sort(arr , mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+    void merge(int arr[], int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        int Left[] = new int [n1];
+        int Right[] = new int [n2];
+        for (int i=0; i<n1; ++i)
+            Left[i] = arr[left + i];
+        for (int j=0; j<n2; ++j)
+            Right[j] = arr[mid + 1+ j];
+        int i = 0, j = 0;
+        int k = left;
+        while (i < n1 && j < n2)
+        {
+            if (Left[i] <= Right[j])
+            {
+                arr[k] = Left[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = Right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1)
+        {
+            arr[k] = Left[i];
+            i++;
+            k++;
+        }
+        while (j < n2)
+        {
+            arr[k] = Right[j];
+            j++;
+            k++;
+        }
+    }
+
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please provide integers as command-line arguments.");
+            return;
+        }
+
+        int[] arr = new int[args.length];
+        for (int i = 0; i < args.length; i++) {
+            try {
+                arr[i] = Integer.parseInt(args[i]);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input: " + args[i] + " is not an integer.");
+                return;
+            }
+        }
+        MergeSort mergeSort = new MergeSort();
+        System.out.println("Unsorted array:");
+        mergeSort.printArray(arr);
+        mergeSort.sort(arr, 0, arr.length - 1);
+        System.out.println("Sorted array:");
+        mergeSort.printArray(arr);
+    }
+}
